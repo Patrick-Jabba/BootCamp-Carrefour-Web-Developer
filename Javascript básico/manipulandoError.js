7d6dbb6be116cc7d1d0dbbe458281bf6d8913c42
Patrick-Jabba <monteiromonterio@gmail.com>
2022-04-07 23:29:24 -0300

    Maps, sets, manipulando Erros

---
+++ Javascript básico/manipulandoError.js
@@ -0,0 +1,35 @@
+// new Error(message, fileName, lineNumber)
+
+// const MeuErro = new Error('Mensagem Inválida');
+// MeuErro.name = 'Invalid Message';
+// throw MeuErro;
+
+function validaArray(arr, num){
+    try{
+        if(!arr && !num) throw new ReferenceError('Envie os parâmetros');
+    
+        if(typeof arr !== 'object') throw new TypeError('Array precisa ser do tipo objeto');
+
+        if(typeof num !== 'number') throw new TypeError('Esse parâmetro não é um número');
+
+        if (arr.length !== num) throw new RangeError('Tamanho inválido');
+        
+        return arr;
+    }
+    catch(e){
+        if(e instanceof ReferenceError){
+            console.log("Este é um Reference Error!")
+            console.log(e.message)
+        } else if(e instanceof TypeError){
+            console.log("Este é um Type Error!")
+            console.log(e.message)
+        } else if(e instanceof RangeError){
+            console.log("Este é um Range Error!")
+            console.log(e.message)
+        }
+        else { console.log('Tipo de erro não esperado: ' + e);
+        }
+    }
+}
+
+console.log(validaArray([], 'oi'));
--- Javascript básico/maps.js
+++ Javascript básico/maps.js
@@ -1,19 +1,35 @@
-function getAdmins(map){
-    let admins = [];
-    for([key, value] of map){
-        if(value === 'Admin'){
-            admins.push(key);
-        }        
+// function getAdmins(map){
+//     let admins = [];
+//     for([key, value] of map){
+//         if(value === 'Admin'){
+//             admins.push(key);
+//         }        
+//     }
+//     return admins;
+// }
+
+// const usuarios = new Map();
+
+// usuarios.set('Luiz', 'Admin');
+// usuarios.set('Stephany', 'Admin');
+// usuarios.set('Jorge', 'User');
+// usuarios.set('Natália', 'Admin');
+
+
+// console.log(getAdmins(usuarios));
+
+function validaArray(arr, tamanho) {
+    try {
+      if (!arr.length || arr.length !== tamanho) throw new RangeError("O tamanho do array é inválido");
+  
+      return arr;
+    } catch(e) {
+      if (e instanceof RangeError) {
+        return console.log(e.message);
+      } else {
+        return e;
+      }
     }
-    return admins;
-}
-
-const usuarios = new Map();
-
-usuarios.set('Luiz', 'Admin');
-usuarios.set('Stephany', 'Admin');
-usuarios.set('Jorge', 'User');
-usuarios.set('Natália', 'Admin');
-
-
-console.log(getAdmins(usuarios));
+  } 
+  
+  validaArray();
