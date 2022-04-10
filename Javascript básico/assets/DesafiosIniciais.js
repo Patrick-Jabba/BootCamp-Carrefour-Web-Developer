6f73e7d153a98446987924bcac421be94354b011
Patrick-Jabba <monteiromonterio@gmail.com>
2022-04-10 12:47:34 -0300

    Desafios iniciais de Js concluídos

--- Javascript básico/assets/DesafiosIniciais.js
+++ Javascript básico/assets/DesafiosIniciais.js
@@ -1,15 +1,16 @@
 // a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados.
 // Abaixo segue um exemplo de código que você pode ou não utilizar
 
-let linhas = parseInt(gets());
+let linhas = parseInt(6);
 
 //TODO: Complete os espaços em branco com uma solução possível para o problema.
 
-let limit = parseInt(gets());
+let limit = parseInt(6);
 
 for (let i = 1; i <= limit; i++) {
-  let X =(   5   );
-  let Y = ( 5 );
-
-  print(   limit   );
+  let x =(i);
+  let y = (i * i);
+  let z = (y * i);
+  console.log(x, y, z);
+  
 }
---
+++ Javascript básico/assets/diasEmAnos.js
@@ -0,0 +1,13 @@
+let totalDeDias = parseInt(400);
+
+let qtdAnos, qtdMeses;
+
+qtdAnos = parseInt(totalDeDias/365);
+totalDeDias = totalDeDias%365;
+
+qtdMeses = parseInt(totalDeDias/30);
+totalDeDias = totalDeDias%30;
+
+let resultado = (qtdAnos + " anos(s) "+ '\n'+qtdMeses+"mes(es)"+'\n'+totalDeDias + "dia(s)");
+
+console.log(resultado);
